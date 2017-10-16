6a394a352768945866bf7ca2ad05be6543abb6e5
ennima <ennima@hotmail.com>
2017-10-16 00:08:59 -0500

    POST 1

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var request = require('request');
 var app = express();
 var php_shit_url = "http://localhost/phps/";
-
+var bodyParser = require('body-parser');
 var sql = require("mssql");
 
+
+//app.use(bodyParser.json());
 // config for your database
 var config = {
     user: 'sa',
@@ -32,9 +34,37 @@ var config = {
     //         sql.close();
     //     });
     // });
+////////////////////////////////  Model Functions //////////////////
 
 
+function db_query(query){
 
+    var resultado = ''; 
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query(query, function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            //res.send(recordset);
+            resultado = recordset;
+            
+            sql.close();
+
+
+        });
+        
+    });
+    console.log(resultado);
+    return resultado;
+}
 
 
 // Esto ayuda a que un HTML externo lo pueda consumir
@@ -44,9 +74,7 @@ app.all('/*', function(req, res, next) {
   next();
 });
 
-app.get('/', function(req, res){
-	res.send("Hola Mundo!");
-});
+
 
 // Prueba si está viendo la base de datos
 app.get('/sql_test', function(req, res){
@@ -74,33 +102,34 @@ app.get('/sql_test', function(req, res){
 });
 
 
+app.post('/login', function(req, res){
+  // var user_rfc = req.params.rfc;
+  // var user_pass = req.params.pass;
+  // db_query('select * from usuarios');
+  // res.send("Eviado: " + user_rfc + user_pass);
+
+  console.log(req.body);
+});
 
 
 
-
-
-
-
-
-
-
-app.get('/nombre/:id', function(req, res) { 
-       if (!req.params.id) { 
-           res.status(500); 
-           res.send({"Error": "Looks like you are not senging the product id to get the product details."}); 
-           console.log("Looks like you are not senging the product id to get the product detsails."); 
-       } 
-      request.get({ url: php_shit_url+"shitsita.php?nombre=" + req.params.id },      function(error, response, body) { 
-              if (!error && response.statusCode == 200) { 
-                  //res.json(body); 
-                  //console.log(body);
-                  var msg = JSON.parse(body);
-                  console.log(msg.edad-10);
-                  var newJson = {"cupon":msg.edad-10,"acceso":"sexshop","color":msg.color};
-                  res.json(newJson);
-                 } 
-             }); 
-     }); 
+// app.get('/login/:id', function(req, res) { 
+//        if (!req.params.id) { 
+//            res.status(500); 
+//            res.send({"Error": "Looks like you are not senging the product id to get the product details."}); 
+//            console.log("Looks like you are not senging the product id to get the product detsails."); 
+//        } 
+//       request.get({ url: php_shit_url+"shitsita.php?nombre=" + req.params.id },      function(error, response, body) { 
+//               if (!error && response.statusCode == 200) { 
+//                   //res.json(body); 
+//                   //console.log(body);
+//                   var msg = JSON.parse(body);
+//                   console.log(msg.edad-10);
+//                   var newJson = {"cupon":msg.edad-10,"acceso":"sexshop","color":msg.color};
+//                   res.json(newJson);
+//                  } 
+//              }); 
+//      }); 
 
 
 /////////  Corre una instancia que sirve por http
